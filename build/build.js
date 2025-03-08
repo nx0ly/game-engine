62930eecc150884120ce2599013a86c07c655ab3
nx0ly <n9d8sa@gmail.com>
2025-03-08 15:16:14 -0500

    first commit

---
+++ build/build.js
@@ -0,0 +1,10 @@
+require("esbuild")
+	.build({
+		entryPoints: ["src/index.ts"],
+		bundle: true,
+		minify: true,
+		sourcemap: true,
+		target: "es2020",
+		outfile: "dist/index.js",
+	})
+	.catch(() => process.exit(1));
