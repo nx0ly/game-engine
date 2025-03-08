b7766b66ba7fc52ccafeaf4ac18ec310ea9cfc84
nx0ly <n9d8sa@gmail.com>
2025-03-08 15:36:12 -0500

    Automatic commit at $(date)

---
+++ src/utils/math.ts
@@ -0,0 +1,188 @@
+import type { Matrix, Vector, UtilsStruct } from "./math.d";
+
+export const {
+	sin,
+	cos,
+	tan,
+	sqrt,
+	PI,
+	abs,
+	round,
+	ceil,
+	random,
+	max,
+	min,
+	log,
+	exp,
+	atan2,
+} = Math;
+
+class MathUtils implements UtilsStruct {
+    radianToDegree(radian: number): number {
+        return radian * 180 / PI;
+    }
+
+    degreeToRadian(degree: number): number {
+        return degree * PI / 180;
+    }
+
+    // ? https://en.wikipedia.org/wiki/Fast_inverse_square_root
+    // TODO: Compare effectiveness of this method with native Math.sqrt()
+    fastInvSqrt(value: number): number {
+        const halfValue = 0.5 * value;
+        let y = value;
+        const threehalfs = 1.5;
+
+        const buf = new ArrayBuffer(4);
+        const f32 = new Float32Array(buf);
+        const u32 = new Uint32Array(buf);
+
+        f32[0] = value;
+        u32[0] = 0x5f3759df - (u32[0] >> 1);
+        y = f32[0];
+
+        y = y * (threehalfs - (halfValue * y * y));
+
+        return y;
+    }
+
+    dotProduct(vector1: Vector, vector2: Vector): number {
+        return vector1.x * vector2.x + vector1.y * vector2.y + vector1.z * vector2.z;
+    }
+
+    crossProduct(vector1: Vector, vector2: Vector): Vector {
+        return {
+            x: vector1.y * vector2.z - vector1.z * vector2.y,
+            y: vector1.z * vector2.x - vector1.x * vector2.z,
+            z: vector1.x * vector2.y - vector1.y * vector2.x,
+        };
+    }
+
+    multiplyMatrix(matrix1: Matrix, matrix2: Matrix): Matrix {
+        const matrix: Matrix = { elements: [] };
+
+        for (let i = 0; i < matrix1.elements.length; i++) {
+            matrix.elements.push([]);
+
+            for (let j = 0; j < matrix2.elements[0].length; j++) {
+                let sum = 0;
+
+                for (let k = 0; k < matrix1.elements[0].length; k++) {
+                    sum += matrix1.elements[i][k] * matrix2.elements[k][j];
+                }
+                
+                matrix.elements[i].push(sum);
+            }
+        }
+
+        return matrix;
+    }
+
+    multiplyVector(matrix: Matrix, vector: Vector): Vector {
+        return {
+            x: matrix.elements[0][0] * vector.x + matrix.elements[0][1] * vector.y + matrix.elements[0][2] * vector.z,
+            y: matrix.elements[1][0] * vector.x + matrix.elements[1][1] * vector.y + matrix.elements[1][2] * vector.z,
+            z: matrix.elements[2][0] * vector.x + matrix.elements[2][1] * vector.y + matrix.elements[2][2] * vector.z,
+        };
+    }
+
+    rotateVector(vector: Vector, angle: number): Vector {
+        const cosAng = cos(angle);
+        const sinAng = sin(angle);
+
+        return {
+            x: vector.x * cosAng - vector.y * sinAng,
+            y: vector.x * sinAng + vector.y * cosAng,
+            z: vector.z,
+        };
+    }
+
+    scaleVector(vector: Vector, scalar: number): Vector {
+        return {
+            x: vector.x * scalar,
+            y: vector.y * scalar,
+            z: vector.z * scalar,
+        };
+    }
+
+    translateVector(vector: Vector, translation: Vector): Vector {
+        return {
+            x: vector.x + translation.x,
+            y: vector.y + translation.y,
+            z: vector.z + translation.z,
+        };
+    }
+
+    transposeMatrix(matrix: Matrix): Matrix {
+        const transposedMatrix: Matrix = { elements: [] };
+
+        for (let i = 0; i < matrix.elements[0].length; i++) {
+            transposedMatrix.elements.push([]);
+
+            for (let j = 0; j < matrix.elements.length; j++) {
+                transposedMatrix.elements[i].push(matrix.elements[j][i]);
+            }
+        }
+
+        return transposedMatrix;
+    }
+
+    determinantMatrix(matrix: Matrix): number {
+        if (matrix.elements.length === 2) {
+            return matrix.elements[0][0] * matrix.elements[1][1] - matrix.elements[0][1] * matrix.elements[1][0];
+        }
+
+        let sum = 0;
+
+        for (let i = 0; i < matrix.elements.length; i++) {
+            const minor: Matrix = { elements: [] };
+
+            for (let j = 1; j < matrix.elements.length; j++) {
+                minor.elements.push([]);
+
+                for (let k = 0; k < matrix.elements.length; k++) {
+                    if (k !== i) {
+                        minor.elements[j - 1].push(matrix.elements[j][k]);
+                    }
+                }
+            }
+
+            sum += matrix.elements[0][i] * this.determinantMatrix(minor) * (i % 2 === 0 ? 1 : -1);
+        }
+
+        return sum;
+    }
+
+    inverseMatrix(matrix: Matrix): Matrix {
+        const determinant = this.determinantMatrix(matrix);
+
+        if (determinant === 0) {
+            throw new Error("matrix is singular");
+        }
+
+        const inverseMatrix: Matrix = { elements: [] };
+
+        for (let i = 0; i < matrix.elements.length; i++) {
+            inverseMatrix.elements.push([]);
+
+            for (let j = 0; j < matrix.elements.length; j++) {
+                const minor: Matrix = { elements: [] };
+
+                for (let k = 0; k < matrix.elements.length; k++) {
+                    if (k !== j) {
+                        minor.elements.push([]);
+
+                        for (let l = 0; l < matrix.elements.length; l++) {
+                            if (l !== i) {
+                                minor.elements[minor.elements.length - 1].push(matrix.elements[k][l]);
+                            }
+                        }
+                    }
+                }
+                inverseMatrix.elements[i].push(this.determinantMatrix(minor) * ((i + j) % 2 === 0 ? 1 : -1) / determinant);
+            }
+        }
+
+        return this.transposeMatrix(inverseMatrix);
+    }
+}
